30796c40c71386e5e92cb14540e129f590dd7032
lyh71709 <60583161+lyh71709@users.noreply.github.com>
2020-04-17 15:12:16 +1200

    Current

---
+++ 04_RPS_round_counter.py
@@ -0,0 +1,24 @@
+# RPS Component 4 - Display round information for the user and also get the max amount of rounds
+
+# To do
+# Check that the user input is an even number so there will never be a tie
+# Ensure that the number of rounds is printed
+
+round_continue = "yes"
+
+while round_continue == "yes":
+    rounds = int(input("The game will be best out of what (Must be an odd number)? "))
+
+    if (rounds % 2) == 1:
+        rounds_played = 0
+
+        while rounds_played < rounds:
+            print("Round {}".format(rounds_played + 1))
+            rounds_played += 1
+
+        print("You have gotten to the end of the game")
+        round_continue = "no"
+
+    else:
+        print("Please enter an odd number as to ensure no ties")
+        print()
